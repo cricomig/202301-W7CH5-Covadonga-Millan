e7cd9dd6828c98b4978571a208e66e4e06fd9180
Covadonga M <cova.millan@gmail.com>
2023-03-04 17:37:06 +0100

    Add config.ts file

---
+++ src/services/config.ts
@@ -0,0 +1,14 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const config = {
+  user: process.env.DB_USER,
+  passwd: process.env.DB_PASSWORD,
+  cluster: process.env.DB_CLUSTER,
+  dbName: process.env.DB_NAME,
+  jwtSecret: process.env.SECRET,
+};
+
+export const __dirname = path.dirname(fileURLToPath(import.meta.url));
