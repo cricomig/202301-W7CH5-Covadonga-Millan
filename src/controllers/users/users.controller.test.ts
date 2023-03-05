3c3e4d1eb7e95f8ef95dcfbb41ff95c551f767ac
Covadonga M <cova.millan@gmail.com>
2023-03-05 18:39:16 +0100

    Add users controller test

---
+++ src/controllers/users/users.controller.test.ts
@@ -0,0 +1,85 @@
+import { Response, Request } from 'express';
+import { UsersMongoRepo } from '../../repository/users/users.mongo.repo.js';
+import { Auth } from '../../helpers/auth.js';
+import { UsersController } from './users.controller.js';
+
+describe('Given UsersController', () => {
+  const repo: UsersMongoRepo = {
+    create: jest.fn(),
+    query: jest.fn(),
+    search: jest.fn(),
+    queryId: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  };
+
+  Auth.hash = jest.fn();
+  Auth.compare = jest.fn();
+  Auth.createJWT = jest.fn();
+
+  let req = {} as unknown as Request;
+  const resp = {
+    json: jest.fn(),
+    status: jest.fn(),
+  } as unknown as Response;
+  const next = jest.fn();
+
+  const controller = new UsersController(repo);
+
+  beforeEach(() => {
+    req = {
+      body: {
+        email: 'cova',
+        passwd: 'eee',
+      },
+    } as unknown as Request;
+  });
+
+  describe('When register is used', () => {
+    test("Then if there's no email or password next should've been called", async () => {
+      req.body.email = null;
+      await controller.register(req, resp, next);
+      expect(next).toHaveBeenCalled();
+    });
+
+    test('Then if all fields are correct it should call resp.json', async () => {
+      await controller.register(req, resp, next);
+
+      (Auth.hash as jest.Mock).mockResolvedValue('some');
+      expect(repo.create).toHaveBeenCalled();
+      expect(resp.status).toHaveBeenCalled();
+      expect(resp.json).toHaveBeenCalled();
+    });
+  });
+
+  describe('When login is called', () => {
+    test("Then if there's no email or password next should be called", async () => {
+      req.body.passwd = '';
+      await controller.login(req, resp, next);
+      expect(next).toHaveBeenCalled();
+    });
+
+    test("Then if there's password and email it should search for user and return a token", async () => {
+      await controller.login(req, resp, next);
+      (repo.search as jest.Mock).mockResolvedValue([2]);
+      (Auth.compare as jest.Mock).mockResolvedValue(true);
+      (Auth.createJWT as jest.Mock).mockResolvedValue('token');
+      expect(resp.json).toHaveBeenCalled();
+    });
+
+    test("Then if there's a password and email and doesn't find a match it should throw an error", async () => {
+      await controller.login(req, resp, next);
+
+      (repo.search as jest.Mock).mockResolvedValue([]);
+
+      expect(next).toHaveBeenCalled();
+    });
+    test('some3', async () => {
+      await controller.login(req, resp, next);
+      (repo.search as jest.Mock).mockResolvedValue([true]);
+      (Auth.compare as jest.Mock).mockResolvedValue(false);
+
+      expect(next).toHaveBeenCalled();
+    });
+  });
+});
---
+++ src/controllers/users/users.controller.ts
@@ -0,0 +1,72 @@
+import { Response, Request, NextFunction } from 'express';
+import { Repo } from '../../repository/repository.interface.js';
+import createDebug from 'debug';
+import { HTTPError } from '../../errors/error.js';
+import { Auth, TokenPayload } from '../../helpers/auth.js';
+import { User } from '../../entities/user.js';
+const debug = createDebug('CH7:controller:users');
+
+export class UsersController {
+  // eslint-disable-next-line no-unused-vars
+  constructor(public repoUsers: Repo<User>) {
+    debug('Instantiate');
+  }
+
+  async getAll(_req: Request, resp: Response, next: NextFunction) {
+    try {
+      debug('getAll');
+      const data = await this.repoUsers.query();
+      resp.json({
+        results: data,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async register(req: Request, resp: Response, next: NextFunction) {
+    try {
+      debug('register:post');
+      if (!req.body.email || !req.body.passwd)
+        throw new HTTPError(401, 'Unauthorized', 'Invalid Email or password');
+      req.body.passwd = await Auth.hash(req.body.passwd);
+      req.body.friends = [];
+      req.body.enemies = [];
+      const data = await this.repoUsers.create(req.body);
+      resp.status(201);
+      resp.json({
+        results: [data],
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async login(req: Request, resp: Response, next: NextFunction) {
+    try {
+      debug('login:post');
+      if (!req.body.email || !req.body.passwd)
+        throw new HTTPError(401, 'Unauthorized', 'Invalid Email or password');
+      const data = await this.repoUsers.search({
+        key: 'email',
+        value: req.body.email,
+      });
+      if (!data.length)
+        throw new HTTPError(401, 'Unauthorized', 'Email not found');
+      if (!(await Auth.compare(req.body.passwd, data[0].passwd)))
+        throw new HTTPError(401, 'Unauthorized', 'Password not match');
+      const payload: TokenPayload = {
+        id: data[0].id,
+        email: data[0].email,
+        role: 'admin',
+      };
+      const token = Auth.createJWT(payload);
+      resp.status(202);
+      resp.json({
+        token,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
