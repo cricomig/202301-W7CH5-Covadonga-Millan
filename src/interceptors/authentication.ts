0e0968b739317f8a19a901ddf6b4e8263a8facb3
Covadonga M <cova.millan@gmail.com>
2023-03-05 18:39:52 +0100

    Modify authentication

--- src/interceptors/authentication.ts
+++ src/interceptors/authentication.ts
@@ -1,25 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
-import { HTTPError } from '../errors/error.js';
 import { Auth, TokenPayload } from '../helpers/auth.js';
-
-export interface RequestCool extends Request {
+import createDebug from 'debug';
+import { HTTPError } from '../errors/error.js';
+const debug = createDebug('W6:interceptor:logged');
+export interface RequestPlus extends Request {
   info?: TokenPayload;
 }
 
-export function authentication(
-  req: RequestCool,
-  resp: Response,
-  next: NextFunction
-) {
-  const authHeader = req.get('Authorization');
-
+export function logged(req: RequestPlus, resp: Response, next: NextFunction) {
   try {
+    debug('Called');
+    const authHeader = req.get('Authorization');
     if (!authHeader)
-      throw new HTTPError(498, 'Token expired', 'No value in http header');
-
+      throw new HTTPError(498, 'Token invalid', 'Not value in auth header');
     if (!authHeader.startsWith('Bearer'))
       throw new HTTPError(498, 'Token invalid', 'Not Bearer in auth header');
-
     const token = authHeader.slice(7);
     const payload = Auth.verifyJWT(token);
     req.info = payload;
